cfc334274977dc718bb1c5ae3c849edb6589c186
TDJR007 <tanwardevansh007@gmail.com>
2025-04-19 20:46:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import predict
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -7,25 +8,20 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Get form data, make prediction
         try:
             data = request.get_json()
             size = float(data['size'])
             year = int(data['year'])
-            view = (data['view'])
-
-            #print(request.form)
+            view = data['view']
 
             prediction = predict.predict_price(size=size, year=year, view=view)
             return jsonify({"prediction": prediction})
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
-            return render_template("index.html", prediction=predicted_value)
 
-    # If method is GET
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
